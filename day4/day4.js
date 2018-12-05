44e956320134e35efb567978e4713440520d66e9
Lukasz Brodowski <lukasz.brodowski@espn.com>
2018-12-05 00:23:20 -0500

    bad code for day4, doesn't work but i want to do day5

---
+++ day4/day4.js
@@ -0,0 +1,89 @@
+const { read } = require('../index');
+
+//-
+//-
+//-  I did this completely wrong. Need to redo it.
+//-
+//-
+
+/**
+ * https://adventofcode.com/2018/day/4
+ *
+ *
+ * Going to assume that guards fall asleep only when time 00.
+ *
+ *
+ * @TODO: You read this really wrong... BACK TO DRAWING BOARD!
+ */
+
+console.log('--- Day 4: Repose Record ---');
+
+const parse = (input) => input.split('\n').sort();
+
+
+const input = parse(read(`${__dirname}/input.txt`));
+
+console.log(input);
+
+// sleep records
+const records = [];
+
+// regex to test what stage input is
+const wakesUp = new RegExp(/wakes\sup/);
+const fallsAsleep = new RegExp(/falls\sasleep/);
+const beginsShift = new RegExp(/begins\sshift/);
+
+// get time and guard
+const getTime = new RegExp(/\:(\d{2})\]/);
+const guardId = new RegExp(/Guard\s\#(\d+)/);
+
+// going to use this obj to build sleep records
+// we can do this cause we assume that they have to
+// a. start shift first
+// b. fall asleep after shift start
+// c. wake up after falling asleep
+// d. repeat b-c until next guard starts shift
+let builder = {};
+
+input.forEach(record => {
+
+    const min = getTime.exec(record)[1];
+
+    switch(true) {
+        case beginsShift.test(record):
+            builder.id = guardId.exec(record)[1];
+            break;
+
+        case fallsAsleep.test(record):
+            // push start time
+            builder.start = parseInt(min);
+            break;
+
+        case wakesUp.test(record):
+            // calculate difference in time
+            // create record
+            records.push({
+                id: builder.id,
+                asleep: parseInt(min) - 1 - builder.start
+            });
+
+            // we don't reset builder here bkz we want to keep
+            // the ID there incase guard keeps falling asleep
+            // waking up
+            break;
+    }
+});
+
+// get max number of hours asleep
+// start with first zzz...
+let sleepiest = records[0];
+
+records.forEach(record => {
+    if(record.asleep > sleepiest.asleep) {
+        sleepiest = record;
+    }
+});
+
+console.log('Part 1:');
+console.log(`The sleepiest guard is #${sleepiest.id} being asleep ${sleepiest.asleep} minutes.`);
+console.log(`Which means the answer is (${sleepiest.id}x${sleepiest.asleep}): `, sleepiest.id * sleepiest.asleep);
